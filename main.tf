12222de4b76a3cc1c3411a629bc58e8ba3c0e84f
Fred Suh <50889806+heessuh@users.noreply.github.com>
2023-02-15 16:47:43 -0300

    updated

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
-resource "aws_vpc" "mtc_vpc" {
-  cidr_block = "10.123.0.0/16"
-
-  tags = {
-    Name = "dev"
-  }
+# Terraform Block
+terraform {
+  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  } 
+}  
+# Provider Block
+provider "aws" {
+  region = "us-east-1"
 }
+
+/*
+Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+$HOME/.aws/credentials
+*/
